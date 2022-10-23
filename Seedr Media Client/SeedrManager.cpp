a2b57763cc9abc0904774daa3b336436ea573030
KRVPerera <rukshan.viduranga@gmail.com>
2022-10-23 10:08:12 +0530

    build: remove cpp modules

---
+++ Seedr Media Client/SeedrManager.cpp
@@ -0,0 +1,26 @@
+#include <string>
+//#include "curl/curl.h"
+
+namespace SeedrManager {
+
+	class SeedrMan {
+
+		//SeedrMan(std::string password, std::string username) : _password(password), _username(_username) {
+
+		//}
+
+
+		//std::string getRootFolder() {
+		//	return "";
+		//}
+
+
+	private:
+		//std::string _password;
+		//std::string _username;
+	};
+
+	void MyFunc()
+	{
+	}
+}
